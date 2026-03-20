ba396f19de28c0dd61b37a33df82191e855836c2
nurbakyt <nurbakyt.sagidulla@narxoz.kz>
2026-03-20 22:26:33 +0500

    implement basic reward system

--- src/com/narxoz/rpg/facade/RewardService.java
+++ src/com/narxoz/rpg/facade/RewardService.java
@@ -2,10 +2,26 @@ package com.narxoz.rpg.facade;
 
 public class RewardService {
     public String determineReward(AdventureResult battleResult) {
-        // TODO: Decide reward rules based on battle outcome.
         if (battleResult == null) {
-            return "TODO";
+            return "No reward";
         }
-        return "TODO";
+
+        if ("Draw".equalsIgnoreCase(battleResult.getWinner())) {
+            return "Broken relic shard";
+        }
+
+        if (battleResult.getWinner() == null || "No one".equalsIgnoreCase(battleResult.getWinner())) {
+            return "No reward";
+        }
+
+        if (battleResult.getRounds() <= 3) {
+            return "Legendary chest";
+        } else if (battleResult.getRounds() <= 6) {
+            return "Epic weapon";
+        } else if (battleResult.getWinner().equals("Shadow Dragon")) {
+            return "Consolation potion";
+        } else {
+            return "Gold and crafting materials";
+        }
     }
 }
